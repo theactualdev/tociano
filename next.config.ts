dd8f38ecda0e199978ff171b5d641fefa5b74d01
theactualdev <olayinkacodes@gmail.com>
2025-05-05 19:17:12 +0100

    changes

---
+++ next.config.ts
@@ -0,0 +1,10 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  compiler: {
+    removeConsole: true
+  }
+};
+
+export default nextConfig;
