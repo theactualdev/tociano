62e0502f094656a871afffeafd62b78a7b7c83dd
theactualdev <olayinkacodes@gmail.com>
2025-05-08 16:36:36 +0100

    fix: fixes metadata

---
+++ app/account/metadata.ts
@@ -0,0 +1,14 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "My Account | Tociano Boutique",
+  description: "Manage your Tociano Boutique account, view orders, update settings, and more.",
+  openGraph: {
+    title: "My Account | Tociano Boutique",
+    description: "Manage your Tociano Boutique account, view orders, update settings, and more.",
+  },
+  twitter: {
+    title: "My Account | Tociano Boutique",
+    description: "Manage your Tociano Boutique account, view orders, update settings, and more.",
+  }
+}; 
